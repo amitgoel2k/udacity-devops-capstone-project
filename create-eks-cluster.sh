539ecd5a04d0d172aa1f7ad86b59f7f063a3d093
Amit Goel <amitgoel.2k@gmail.com>
2022-11-27 16:08:22 +0530

    Added eks cluster create job

---
+++ create-eks-cluster.sh
@@ -0,0 +1,7 @@
+## curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+## Move the extracted binary to /usr/local/bin
+sudo mv /tmp/eksctl /usr/local/bin
+
+## Create nodegroup 
---
+++ setup-iam-role.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+EKS_ROLE_NAME=$1
+EKS_PROFILE_NAME=udacity-eks
+CFN_STACK_NAME=eksctl-udacity-cluster
+EKS_ADMIN_USER=udacity-eks-admin-user
+AWS_REGION=us-east-1
+
+aws cloudformation deploy \
+  --template-file ../config/iam/prepare-iam-cfn.yaml \
+  --stack-name $CFN_STACK_NAME \
+  --parameter-overrides EKSUserName=$EKS_ADMIN_USER EKSRoleName=$EKS_ROLE_NAME \
+  --region $AWS_REGION \
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" \
+  --tags project=udacity-capstone
+ 
+
+# ~/.aws/config [default]
+aws configure set default.output json
+aws configure set default.region $AWS_REGION
+
+# ~/.aws/config [profile uda-eks-admin]
+echo "retrieving eks role arn..."
+EKS_ADMIN_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name $CFN_STACK_NAME --region $AWS_REGION --query "Stacks[0].Outputs[?OutputKey=='EKSAdminRoleARN'].OutputValue" --output text)
+
+echo $EKS_ADMIN_ROLE_ARN
+
+aws configure set profile.$EKS_PROFILE_NAME.output json
+aws configure set profile.$EKS_PROFILE_NAME.region $AWS_REGION
+aws configure set profile.$EKS_PROFILE_NAME.role_arn $EKS_ADMIN_ROLE_ARN
+aws configure set profile.$EKS_PROFILE_NAME.source_profile default
+
+# set ~/.aws/credentials and ~/.aws/config
+CREATE_ACCESS_KEY_OUTPUT=$(aws iam create-access-key --user-name $EKS_ADMIN_USER)
+ACCESS_KEY_ENVIRONMENT=$(echo $CREATE_ACCESS_KEY_OUTPUT | jq -r '.AccessKey | .["AWS_ACCESS_KEY_ID"] = .AccessKeyId | .["AWS_SECRET_ACCESS_KEY"] = .SecretAccessKey | del(.AccessKeyId, .SecretAccessKey, .UserName, .Status, .CreateDate) | to_entries[] | "export \(.key)=\(.value)"')
+
+#echo "CREATE_ACCESS_KEY_OUTPUT=$CREATE_ACCESS_KEY_OUTPUT"
+#echo "ACCESS_KEY_ENVIRONMENT=$ACCESS_KEY_ENVIRONMENT"
+
+eval $ACCESS_KEY_ENVIRONMENT
+
+#echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
+#echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
+
+# ~/.aws/credentials [default]
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
