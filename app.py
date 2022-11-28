a1ec93abef3c44e2dccb38baee93df148822462e
Amit Goel <amitgoel.2k@gmail.com>
2022-11-28 17:31:25 +0530

    Added screenshots

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Mike! Great to see you deployed the third version of this service'
+    return 'Hello, Amit! Great that you have deployed this in AWS'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
